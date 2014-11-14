74d30d07cdc334b8a37b59637cda52686a440302
Inkubus <Inkubus@inku-hp-win7>
2014-11-14 19:31:06 +0100

    komentar 1

--- I'mLost/src/com/imlost/imlost/MainActivity.java
+++ I'mLost/src/com/imlost/imlost/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+// @inkubzz komentar 1
 public class MainActivity extends ActionBarActivity {
 
     @Override
